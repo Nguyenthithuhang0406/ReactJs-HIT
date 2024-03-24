50cc0027f244be92df3a517877b674c5c0649559
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-03-25 01:33:22 +0700

    BTVN1+3

---
+++ LiThuyet/Card2/Card.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import kkkk from "../../../assets/img/kkkkk.jpg";
+import "./Card.scss";
+
+const Card = () => {
+  return (
+    <>
+    <div className='card2'>
+        <img src={kkkk}/>
+        <h1>kkkkkk
+        <span>
+            <a>Hello</a>
+        </span>
+        </h1>
+        <p>hduwefgskcvsjcvskfwkhfwk  jugfbjffikw qjjqdahbabhergiu  fbwjfkwqbqjf</p>
+    </div>
+    </>
+  )
+}
+
+export default Card;
---
+++ note.md
@@ -0,0 +1,7 @@
+do uu tien trong css
+important    : oo
+inline       :1000
+id           :100
+class        :10
+tag          : 2
+*            :1
